5d6f3523eeea47ffd90c93f557ab0ed382b63f9b
Agustin <agustinjimenezpolonio@gmail.com>
2020-12-15 15:26:30 +0100

    Mi primer Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let app = express();
+let bodyParser  = require("body-parser");
+let db=require("./database");
+let port=3000;
+
+app.use(bodyParser.urlencoded({ limit:'50mb',extended: true }));
+app.use(bodyParser.json({ limit:'50mb',extended: true }));
+
+let router=express.Router();
+
+router.get('/clientes',db.getAllClients);
+router.get('/clientes/:id',db.getClientById);
+
+app.use(router);
+app.listen(process.env.PORT || port,()=>{
+    console.log("Arrancando en http://localhost:3000");
+})
